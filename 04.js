7fabc69417dc24e49d57a7e62bf7aaf12cbd0bcc
Nina <ninahovinmaa@gmail.com>
2020-10-09 09:17:08 +0200

    added solution to q4

--- 04.js
+++ 04.js
@@ -1,14 +1,22 @@
 /*
 
-Gör en funktion: get_average_sum. Funktionen ska ta emot en array som parameter och räkna ut genomsnittet och summan av talen du skickade in och returnera de två svaren till den som anropar metoden. Du ska returnera svaren som ett objekt med egenskaperna average och sum. Du ska inte skriva någon klass utan bara returnera ett "fristående" objekt.
+Gör en funktion: get_average_sum. Funktionen ska ta emot en array som parameter 
+och räkna ut genomsnittet och summan av talen du skickade in och returnera de 
+två svaren till den som anropar metoden. Du ska returnera svaren som ett objekt
+ med egenskaperna average och sum. Du ska inte skriva någon klass utan bara returnera
+  ett "fristående" objekt.
 
 Du måste använda en reduce-funktion.
 
-Anropa get_average_sum och skicka med en array med 2 till 10 heltal. Kontrollera antalet tal och returnera false om antalet tal är fel eller om ett värde inte är ett heltal.
+Anropa get_average_sum och skicka med en array med 2 till 10 heltal. Kontrollera
+ antalet tal och returnera false om antalet tal är fel eller om ett värde inte är
+  ett heltal.
 
-Skriv kod som anropar get_average_sum, sparar resultatet i en variabel och skriver ut båda värdena.
+Skriv kod som anropar get_average_sum, sparar resultatet i en variabel och skriver
+ ut båda värdena.
 
-OBS! get_average_sum-funktionen i sig får inte innehålla utskrifter, utskriften ska ske utanför funktionen.
+OBS! get_average_sum-funktionen i sig får inte innehålla utskrifter, utskriften ska 
+ske utanför funktionen.
 
 Exempel på värden och deras svar:
 
@@ -18,7 +26,28 @@ Exempel på värden och deras svar:
 
 */
 
-function get_average_sum() {
-    
+sum = 0;
+average = 0;
+
+function get_average_sum(arr) {
+    for (let element of arr) {
+       
+        sum += element;
+    }
+
+    const reducer = (accumulator, item) => {
+        return accumulator + item;
+        };
+
+    let reducedArr = arr.reduce(reducer, 0);
+    average = reducedArr/arr.length;
+
+    let obj = {
+        average: average,
+        sum: sum
+    }
+
+    returnobj;   
 }
 
+get_average_sum(arr)
