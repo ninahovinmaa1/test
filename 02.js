e918114ceefe38e06a135cdea8520cbf787fc017
Nina <ninahovinmaa@gmail.com>
2020-10-09 11:23:47 +0200

    started solutions for q02 and q03

--- 02.js
+++ 02.js
@@ -1,10 +1,13 @@
 /*
 
-Skriv en funktion som tar en sträng som parameter, gör om alla tecken som inte är a-z (stora eller små) till HTML-entiteter och returnerar resultatet.
+Skriv en funktion som tar en sträng som parameter, gör om alla tecken som inte är 
+a-z (stora eller små) till HTML-entiteter och returnerar resultatet.
 
 Läs mer om HTML-entiteter här: https://dev.w3.org/html5/html-author/charref
 
-Din funktion måste byta ut alla "osäkra" tecken mot "säkra". De tecken ni behöver byta ut är:
+Din funktion måste byta ut alla "osäkra" tecken mot "säkra". De tecken ni behöver 
+byta ut är:
+
 <
 >
 å
@@ -12,11 +15,17 @@ Din funktion måste byta ut alla "osäkra" tecken mot "säkra". De tecken ni beh
 ö
 '
 
-Alla tecken som inte är a-z eller någon av ovanstående (t ex kommatecken, punkter, bindestreck osv) ska tas bort.
+Alla tecken som inte är a-z eller någon av ovanstående (t ex kommatecken, punkter,
+ bindestreck osv) ska tas bort.
 
 */
 
-function safe_string() {
+var str1 = "<Sjörövare>"
+
+function safe_string(string) {
+    for (let i = 0; i < string.length; i++) {
+        console.log(str[i])
+    }
 
 }
 
